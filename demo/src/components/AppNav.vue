1f2bf1584cc107e74887e911f0b4ee56e47beb74
Marcus Lindfeldt <marcus.lindfeldt@gmail.com>
2016-09-09 12:15:35 +0200

    Make the app nav more responsive

--- demo/src/components/AppNav.vue
+++ demo/src/components/AppNav.vue
@@ -1,14 +1,19 @@
 <template lang="html">
   <nav class="navbar navbar-full navbar-dark bg-inverse">
+    <button class="navbar-toggler bg-inverse hidden-sm-up pull-xs-right" type="button" data-toggle="collapse" data-target="#appnav" aria-controls="appnav" aria-expanded="false" aria-label="Toggle navigation">
+      &#9776;
+    </button>
     <a class="navbar-brand" href="#">Jayway shop</a>
-    <ul class="nav navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" v-link="{ path: '/' }" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" v-link="{ path: '/manage-products' }" href="#">Manage Products</a>
-      </li>
-    </ul>
+    <div class="collapse navbar-toggleable-xs" id="appnav">
+      <ul class="nav navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" v-link="{ path: '/' }" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" v-link="{ path: '/manage-products' }" href="#">Manage Products</a>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
@@ -23,4 +28,9 @@ export default {
 </script>
 
 <style lang="css" scoped>
+@media (max-width: 543px) {
+  .navbar-toggleable-xs {
+    clear: left;
+  }
+}
 </style>
