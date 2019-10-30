f0c3201c3788b7d53a8720291fb53d4778e95d30
Shashank Shekhar <shashank7200@gmail.com>
2019-10-31 00:44:01 +0530

    added manage product as the root component

--- exercise-files/vue-list-products/src/App.vue
+++ exercise-files/vue-list-products/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <manage-products/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ManageProducts from './components/ManageProducts.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ManageProducts
   }
 }
 </script>
