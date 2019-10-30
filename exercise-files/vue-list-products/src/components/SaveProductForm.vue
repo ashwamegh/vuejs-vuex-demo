e1a44113139211a001bc8bab235d72a6f1bb9601
Shashank Shekhar <shashank7200@gmail.com>
2019-10-31 00:45:05 +0530

    created save product form for saving into products array

---
+++ exercise-files/vue-list-products/src/components/SaveProductForm.vue
@@ -0,0 +1,29 @@
+<!-- src/components/SaveProductForm.vue  -->
+<template>
+  <form>
+    <div class="form-group">
+      <label for="productName">Product name</label>
+      <input type="text" v-model="product.name" class="form-control" id="productName" maxlength="32" placeholder="Enter product name">
+    </div>
+    <div class="form-group">
+      <label for="productDescription">Product description <small class="text-muted">(optional)</small></label>
+      <textarea class="form-control" v-model="product.description" id="productDescription" rows="3" maxlength="128" placeholder="Enter description"></textarea>
+    </div>
+    <div class="form-group">
+      <label for="price">Price</label>
+      <input type="number" v-model="product.price" class="form-control" id="price" placeholder="Enter Price" number>
+    </div>
+    <button type="submit" v-on:click.prevent="onSubmit" class="btn btn-primary">Save product</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ['product'],
+  methods: {
+    onSubmit () {
+      this.$emit('submit', this.product)
+    }
+  }
+}
+</script>
