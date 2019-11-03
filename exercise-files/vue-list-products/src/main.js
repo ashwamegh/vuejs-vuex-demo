a15b487adb2b98b44a745569a7f3469f7b13ef80
Shashank Shekhar <shashank7200@gmail.com>
2019-11-03 14:09:32 +0530

    updated vue middleware

--- exercise-files/vue-list-products/src/main.js
+++ exercise-files/vue-list-products/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 import store from './vuex/store'
 
@@ -13,7 +14,9 @@ import './styles/style.scss'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
+Vue.http.options.root = 'http://localhost:3000'
 const routes = [
   { path: '/home', alias:'/', component: ProductCatalog},
   { path: '/manage-products', component: ManageProducts}
