ea87cb0a33f20e61c12b1f173a091df870116bd1
Shashank Shekhar <shashank7200@gmail.com>
2019-11-03 14:10:20 +0530

    created new actions to communicate data with api

--- exercise-files/vue-list-products/src/vuex/modules/products/actions.js
+++ exercise-files/vue-list-products/src/vuex/modules/products/actions.js
@@ -1,23 +1,40 @@
-import uuid from 'uuid'
+import Vue from "vue";
 
 import {
   DELETE_PRODUCT,
   CREATE_PRODUCT,
-  UPDATE_PRODUCT
-} from './types'
+  UPDATE_PRODUCT,
+  FETCH_PRODUCTS
+} from "./types";
+
+export function fetchProducts({ commit }) {
+  Vue.http
+    .get("products/")
+    .then(response => commit(FETCH_PRODUCTS, response.body.data));
+}
+
+export function createProduct ({ commit }, product) {
+  return Vue.http.post('products', product)
+    .then((response) => commit(CREATE_PRODUCT, response.body.data))
+}
+
+export function updateProduct ({ commit }, product) {
+  return Vue.http.put(`products/${product.id}`, product)
+    .then((response) => commit(UPDATE_PRODUCT, response.body.data))
+}
+
+export function deleteProduct ({ commit }, productId) {
+  return Vue.http.delete(`products/${productId}`)
+    .then(() => commit(DELETE_PRODUCT, productId))
+}
 
 export function saveProduct ({ commit, state }, product) {
   const index = state.all.findIndex((p) => p.id === product.id)
 
   // update product if it exists or create it if it doesn't
   if (index !== -1) {
-    commit(UPDATE_PRODUCT, product)
+    return updateProduct({ commit }, product)
   } else {
-    product.id = uuid.v4()
-    commit(CREATE_PRODUCT, product)
+    return createProduct({ commit }, product)
   }
-}
-
-export function deleteProduct ({ commit }, productId) {
-  commit(DELETE_PRODUCT, productId)
 }
